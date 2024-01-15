1e7fd2b02b272da08b715923284b623cf9c0229c
ASUS Desktop <1033718596@qq.com>
2024-01-15 13:04:17 -0800

    batch size multi cards

--- mycode/llava_rel.py
+++ mycode/llava_rel.py
@@ -3,7 +3,7 @@ import os
 import copy
 import json
 import sys
-from tqdm import tqdm
+from tqdm import tqdm, trange
 
 import numpy as np
 import torch
@@ -18,51 +18,76 @@ all_obj = set("person,broom,picture,closet,cabinet,blanket,window,table,paper,no
 
 # os.environ['TRANSFORMERS_CACHE'] = '/nobackup/users/bowu/model/transformers_cache'
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser()
+parser.add_argument('--image_root_path', type=str, default='/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/Charades_v1_480/', help='path all images')
+parser.add_argument('--image_info_path', type=str, default='/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/objects.json', help='path all images')
+parser.add_argument('--output_path', type=str, default='/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/llava_', help='path to save annotations')
+parser.add_argument('--batch_size', type=int, default=2)
+parser.add_argument('--process_num', type=int, default=16)
+parser.add_argument('--rank', type=int, default=2)
+args = parser.parse_args()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--image_root_path', type=str, default='/home/azon/data/video/', help='path all images')
-    parser.add_argument('--image_info_path', type=str, default='./objects.json', help='path all images')
-    parser.add_argument('--output_path', type=str, default='llava.json', help='path to save annotations')
-    args = parser.parse_args()
+print('load model')
+model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
+processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
+model.to(0)
 
-    print('load model')
-    model = LlavaForConditionalGeneration.from_pretrained("llava-hf/llava-1.5-7b-hf")
-    processor = AutoProcessor.from_pretrained("llava-hf/llava-1.5-7b-hf")
-    model.to(0)
+# Use this command for evaluate the Grounding DINO model
+# Or you can download the model by yourself
+print('load data')
+objects = json.load(open(args.image_info_path, 'r'))
+key_list = []
+for video_id, video_info in objects.items():
+    for frame_id, frame_info in video_info.items():
+        key_list.append((video_id, frame_id))
 
-    # Use this command for evaluate the Grounding DINO model
-    # Or you can download the model by yourself
-    print('load data')
-    objects = json.load(open(args.image_info_path, 'r'))
+klen = len(key_list)
+rank_num = klen // args.process_num
+rank_start = rank_num * args.rank
+rank_end = rank_num * (args.rank + 1)
+print('rank_start', rank_start, 'rank_end', rank_end)
+key_list = key_list[rank_start: rank_end]
 
-    dlength = len(os.listdir(args.image_root_path))
-    pbar = tqdm(os.walk(args.image_root_path), total=dlength, leave=True)
-    for root, dirs, files in pbar:
-        for name in tqdm(files, leave=False):
-            image_path = os.path.join(root, name)
-            pbar.set_description("Processing %s" % image_path)
-            video_id = image_path.split('/')[-2].split('.')[-2]
-            frame_id = image_path.split('/')[-1].split('.')[-2]
-            labels = objects[video_id][frame_id]['labels']
+resf = open(args.output_path + str(args.rank) + '.txt', 'w')
+pbar = trange(len(key_list) // args.batch_size + 1)
 
-            filtered = set()
-            for item in labels:
-                arr = item.split(' ')
-                if arr[0] in all_obj:
-                    filtered.add(arr[0])
-            filtered = list(filtered)
-
-            llava_prompt = "<image>\nFind important positional relations among only these objects: " + ','.join(filtered) + "\nASSISTANT:"
-            image = Image.open(image_path)
-            inputs = processor(text=llava_prompt, images=image, return_tensors="pt").to(0)
-            generate_ids = model.generate(**inputs, max_length=512, temperature=0.1, top_p=0.7, do_sample=True)
-            return_text = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_token0104zation_spaces=False)[0]
-
-            objects[video_id][frame_id]['llava'] = return_text
-
+for _ in pbar:
     
-    json.dump(objects, open(args.output_path, 'w'))
+    image_list = []
+    prompt_list = []
+    batch_list = []
+    for i in range(args.batch_size):
+        if len(key_list) == 0:
+            break
+        video_id, frame_id = key_list.pop()
+        image_path = os.path.join(args.image_root_path, video_id + '.mp4', frame_id + '.png')
+        pbar.set_description("Processing %s" % image_path)
+    
+        labels = objects[video_id][frame_id]['labels']
+
+        filtered = set()
+        for item in labels:
+            arr = item.split(' ')
+            if arr[0] in all_obj:
+                filtered.add(arr[0])
+        filtered = list(filtered)
+
+        prompt_list.append("<image>\nFind important positional relations among only these objects: " + ','.join(filtered) + "\nASSISTANT:")
+        image_list.append(Image.open(image_path))
+        batch_list.append((video_id, frame_id))
+
+    inputs = processor(text=prompt_list, images=image_list, return_tensors="pt", padding=True).to(0)
+    generate_ids = model.generate(**inputs, max_length=512, temperature=0.1, top_p=0.7, do_sample=True)
+    returns = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_token0104zation_spaces=False)
+
+    for (video_id, frame_id), return_text in zip(batch_list, returns):
+        resf.write(json.dumps([video_id, frame_id, return_text]) + '\n')
+
+    resf.flush()
+    if _ == 20:
+        break
+
+resf.close()
 
 # export HUGGINGFACE_HUB_CACHE=/nobackup/users/bowu/model/transformers_cache/
-# python llava_rel.py --image_root_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/Charades_v1_480/ --image_info_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/objects.json --output_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/llava.json
+# python llava_rel.py --rank=0 --image_root_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/Charades_v1_480/ --image_info_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/objects.json --output_path=/nobackup/users/bowu/data/STAR/Raw_Videos_Frames/llava.txt
